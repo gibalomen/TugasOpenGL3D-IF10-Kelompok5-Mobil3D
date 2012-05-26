db1da8d16754d39b7502be898bf9f0bc72042999
Aditia Purbo Aji <gibalomen@yahoo.co.id>
2012-05-27 06:22:00 +0700

    update mobil ke becak
    becak 3D

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /* 
 
 Tugas Grafika Komputer
-Judul : Mobil 3D
+Judul : Becak 3D revisi dari Mobil 3D
 
 Kelas IF 10
 Kelompok 5 :
@@ -16,56 +16,6 @@ Tombol : Key 'z' dan 'y' untuk rotasi
 #include <windows.h>
 #include <GL/glut.h>
 
-void SetMaterial(GLfloat spec[], GLfloat amb[], GLfloat diff[], GLfloat shin[]);
-//material definitions
-
-
-
-GLfloat mat_specularWHITE[] ={255.0,255.0,255.0,1.0};
-GLfloat mat_ambientWHITE[] ={255.0,255.0,255.0,1.0};
-GLfloat mat_diffuseWHITE[] ={255.0,255.0,255.0,1.0};
-GLfloat mat_shininessWHITE[] ={128.0 * 0.4};
-
-GLfloat mat_specularGRAY[] ={0.75,0.75,0.75,1.0};
-GLfloat mat_ambientGRAY[] ={0.5,0.5,0.5,1.0};
-GLfloat mat_diffuseGRAY[] ={0.50,0.50,0.50,1.0};
-GLfloat mat_shininessGRAY[] ={128.0 * 0.6};
-
-GLfloat mat_specularBLUE[] ={0.75,0.75,0.75,1.0};
-GLfloat mat_ambientBLUE[] ={0,0,1,1.0};
-GLfloat mat_diffuseBLUE[] ={0.50,0.50,0.50,1.0};
-GLfloat mat_shininessBLUE[] ={128.0 };
-
-GLfloat mat_specularGREEN[] ={0.633, 0.727811, 0.633,1.0};
-GLfloat mat_ambientGREEN[] ={0.0215, 0.1745, 0.0215,1.0};
-GLfloat mat_diffuseGREEN[] ={0.07568, 0.61424, 0.07568,1.0};
-GLfloat mat_shininessGREEN[] ={128.0};
-
-GLfloat mat_specularYELLOW[] ={0.75,0.75,0.75,1.0};
-GLfloat mat_ambientYELLOW[] ={1,1,0,1.0};
-GLfloat mat_diffuseYELLOW[] ={0.50,0.50,0.50,1.0};
-GLfloat mat_shininessYELLOW[] ={128.0};
-
-GLfloat mat_specularRED[] ={0.75,0.75,0.75,1.0};
-GLfloat mat_ambientRED[] ={1.0,0.0,0.0,1.0};
-GLfloat mat_diffuseRED[] ={0.50,0.50,0.50,1.0};
-GLfloat mat_shininessRED[] ={128.0};
-
-
-void SetMaterial(GLfloat spec[], GLfloat amb[], GLfloat diff[], GLfloat shin[])
-{
-
-  glMaterialfv(GL_FRONT, GL_SPECULAR, spec);
-  glMaterialfv(GL_FRONT, GL_SHININESS, shin);
-  glMaterialfv(GL_FRONT, GL_AMBIENT, amb);
-  glMaterialfv(GL_FRONT, GL_DIFFUSE, diff);
-}
-
-
-
-
-
-
 
 static float ypoz = 0, zpoz = 0;
 
@@ -77,6 +27,9 @@ void init(void)
     glClearColor (0.0, 0.0, 0.0, 0.0);
     glEnable(GL_DEPTH_TEST);
     glShadeModel (GL_SMOOTH);
+    
+    
+/*
 //lighting
    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
    GLfloat mat_shininess[] = { 50.0 };
@@ -84,13 +37,18 @@ void init(void)
    glClearColor (0.0, 0.0, 0.0, 0.0);
    glShadeModel (GL_SMOOTH);
 
-   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-   glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
+ 
    glEnable(GL_LIGHTING);
    glEnable(GL_LIGHT0);
    glEnable(GL_DEPTH_TEST);
+*/
+glEnable(GL_LIGHTING);
+glEnable(GL_LIGHT0);
+GLfloat light_position[] = { -10.0, 9.0, 0.0, 0.0 };
+GLfloat cyan[] = {0.f, .3f, .1f, 1.f};
+glMaterialfv(GL_FRONT, GL_DIFFUSE, cyan);
+
+
 }
 
 void display(void)
@@ -105,74 +63,184 @@ void display(void)
     glRotatef(ypoz,0,1,0);
     glRotatef(zpoz,0,0,1);
 
-
-
-//roda kiri
-    glPushMatrix();
-	glTranslatef(0.0,-1.2,1.6);
-	glScalef(1.1,1.1,1.1);
-	SetMaterial(mat_specularGRAY, mat_ambientGRAY, mat_diffuseGRAY, mat_shininessGRAY);
-	glutSolidTorus(0.13,0.13,22,22);
-    glPopMatrix();       
-    
-    glPushMatrix();
-	glTranslatef(1.0,-1.2,1.6);
-	glScalef(1.1,1.1,1.1);
-	SetMaterial(mat_specularGRAY, mat_ambientGRAY, mat_diffuseGRAY, mat_shininessGRAY);
-	glutSolidTorus(0.13,0.13,22,22);
-    glPopMatrix(); 
-        
-//roda kanan 
-    glPushMatrix();
-	glTranslatef(1.0,-1.2,2.6);
-	glScalef(1.1,1.1,1.1);
-	SetMaterial(mat_specularGRAY, mat_ambientGRAY, mat_diffuseGRAY, mat_shininessGRAY);
-	glutSolidTorus(0.13,0.13,22,22);
-    glPopMatrix();       
-    
-    glPushMatrix();
-	glTranslatef(0.0,-1.2,2.6);
-	glScalef(1.1,1.1,1.1);
-	SetMaterial(mat_specularGRAY, mat_ambientGRAY, mat_diffuseGRAY, mat_shininessGRAY);
-	glutSolidTorus(0.13,0.13,22,22);
-    glPopMatrix();    
-
-//body
+//body kanan
     glPushMatrix();
 	glTranslatef(0.5,-1.1,2.1);
-	glScalef(1.9,0.5,0.9);
-	SetMaterial(mat_specularYELLOW, mat_ambientYELLOW, mat_diffuseYELLOW, mat_shininessYELLOW);
+	glScalef(1.0,0.8,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+    
+    glPushMatrix();
+	glTranslatef(1.10,-1.28,2.1);
+	glScalef(0.5,0.44,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+
+    glPushMatrix();
+	glTranslatef(1.0,-1.06,2.1);
+	glRotatef(45,0,0,1);
+	glScalef(0.5,0.5,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+
+
+//body kiri
+    glPushMatrix();
+	glTranslatef(0.5,-1.1,1.1);
+	glScalef(1.0,0.8,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+    
+    glPushMatrix();
+	glTranslatef(1.10,-1.28,1.1);
+	glScalef(0.5,0.44,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+
+    glPushMatrix();
+	glTranslatef(1.0,-1.06,1.1);
+	glRotatef(45,0,0,1);
+	glScalef(0.5,0.5,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+    
+//tiang kanan
+    glPushMatrix();
+	glTranslatef(0.5,-0.7,2.1);
+	glScalef(0.05,1.0,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+    
+    glPushMatrix();
+	glTranslatef(0.5,-0.7,2.1);
+	glRotatef(45,0,0,1);
+	glScalef(0.05,1.0,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix(); 
+    
+    glPushMatrix();
+	glTranslatef(0.6,-0.55,2.1);
+	glRotatef(-45,0,0,1);
+	glScalef(0.05,1.0,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix(); 
+    
+//tiang kiri
+    glPushMatrix();
+	glTranslatef(0.5,-0.7,1.1);
+	glScalef(0.05,1.0,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+    
+    glPushMatrix();
+	glTranslatef(0.5,-0.7,1.1);
+	glRotatef(45,0,0,1);
+	glScalef(0.05,1.0,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix(); 
+    
+    glPushMatrix();
+	glTranslatef(0.6,-0.55,1.1);
+	glRotatef(-45,0,0,1);
+	glScalef(0.05,1.0,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix(); 
+
+//body belakang
+    glPushMatrix();
+	glTranslatef(0.0,-1.1,1.6);
+	glScalef(0.05,0.8,1.0);
 	glutSolidCube(1.0);
     glPopMatrix();  
 
 //atap
     glPushMatrix();
-	glTranslatef(0.35,-0.8,2.1);
-	glScalef(0.9,0.5,0.9);
-	SetMaterial(mat_specularYELLOW, mat_ambientYELLOW, mat_diffuseYELLOW, mat_shininessYELLOW);
-	glutSolidCube(1.0);
-    glPopMatrix(); 
- 
- //kaca
-    glPushMatrix();
-	glTranslatef(0.8,-0.9,2.1);
-	glRotatef(45,0,0,1);
-	glScalef(0.5,0.5,0.9);
-	SetMaterial(mat_specularYELLOW, mat_ambientYELLOW, mat_diffuseYELLOW, mat_shininessYELLOW);
-	glutSolidCube(1.0);
-    glPopMatrix(); 
- 
-//wing
-
-    glPushMatrix();
-	glTranslatef(-0.29,-0.8,2.1);
-//	glRotatef(-10,0,0,1);
-	glScalef(0.2,0.05,0.9);
-	SetMaterial(mat_specularYELLOW, mat_ambientYELLOW, mat_diffuseYELLOW, mat_shininessYELLOW);
+	glTranslatef(0.9,-0.2,1.6);
+	glScalef(0.8,0.05,1.1);
 	glutSolidCube(1.0);
     glPopMatrix();  
     
+    glPushMatrix();
+	glTranslatef(0.32,-0.28,1.6);
+	glRotatef(27,0,0,1);
+	glScalef(0.38,0.05,1.1);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+
+//kursi
+    glPushMatrix();
+	glTranslatef(0.5,-1.28,1.6);
+	glScalef(0.85,0.5,1.0);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+
+//roda
+    glPushMatrix();
+	glTranslatef(0.7,-1.3,2.2);
+	glScalef(1.0,1.0,1.0);
+	glutSolidTorus(0.05,0.3,20,20);
+    glPopMatrix(); 
     
+    glPushMatrix();
+	glTranslatef(0.7,-1.3,1.05);
+	glScalef(1.0,1.0,1.0);
+	glutSolidTorus(0.05,0.3,20,20);
+    glPopMatrix(); 
+    
+    glPushMatrix();
+	glTranslatef(-1.0,-1.3,1.6);
+	glScalef(1.0,1.0,1.0);
+	glutSolidTorus(0.05,0.3,20,20);
+    glPopMatrix(); 
+    
+    
+  //tail kiri
+    glPushMatrix();
+	glTranslatef(-0.6,-1.0,1.5);
+	glRotatef(-65,0,0,1);
+	glScalef(0.05,1.2,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+    
+    glPushMatrix();
+	glTranslatef(-0.5,-1.24,1.5);
+	glRotatef(-90,0,0,1);
+	glScalef(0.05,1.2,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+    
+    //tail kanan
+    
+    glPushMatrix();
+	glTranslatef(-0.6,-1.0,1.7);
+	glRotatef(-65,0,0,1);
+	glScalef(0.05,1.2,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+    
+    glPushMatrix();
+	glTranslatef(-0.5,-1.24,1.7);
+	glRotatef(-90,0,0,1);
+	glScalef(0.05,1.2,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix();  
+    
+    glPushMatrix();
+	glTranslatef(-0.2,-1.0,1.7);
+	glRotatef(55,0,0,1);
+	glScalef(0.05,1.2,0.05);
+	glutSolidCube(1.0);
+    glPopMatrix(); 
+    
+    glPushMatrix();
+	glTranslatef(-0.65,-0.65,1.7);
+
+	glScalef(1.1,0.2,0.5);
+	glutSolidSphere(0.2,20,20);
+    glPopMatrix(); 
+    
+    
+
     
     
     Sleep(5);
@@ -241,9 +309,9 @@ int main(int argc, char** argv)
 
     glutInit(&argc, argv);
     glutInitDisplayMode (GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-    glutInitWindowSize (500, 500);
+    glutInitWindowSize (600, 600);
     glutInitWindowPosition (100, 100);
-	glutCreateWindow("Mobil 3D - kelompok 5");
+	glutCreateWindow("Becak 3D - kelompok 5");
     init ();
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
@@ -255,3 +323,4 @@ int main(int argc, char** argv)
     return 0;
 
 } 
+
